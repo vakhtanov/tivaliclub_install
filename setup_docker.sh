abaaad1e67d89ef70f81b4f7bcd818531aa41f87
Andrey Vakhtanov <wahha@mail.ru>
2026-03-31 22:19:58 +0300

    Add files via upload

---
+++ setup_docker.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+set -e
+
+# 1. Set up Docker's apt repository
+apt update
+apt install -y ca-certificates curl
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+# 2. Add the repository to Apt sources
+tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/debian
+Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+apt update
+
+# 3. Install docker
+echo "---===*** Install docker ***===---"
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# 4. Check docker status
+ systemctl status docker
