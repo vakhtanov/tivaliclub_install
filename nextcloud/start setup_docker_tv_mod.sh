a705897e71aa42faf9c98f3507ec5abec3e602ed
Andrey Vakhtanov <wahha@mail.ru>
2026-04-03 13:41:50 +0300

    Update start setup_docker_tv_mod.sh

--- nextcloud/start setup_docker_tv_mod.sh
+++ nextcloud/start setup_docker_tv_mod.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
-set -e
+sudo set -e
 
 # 1. Set up Docker's apt repository
-apt update
-apt install -y ca-certificates curl
-install -m 0755 -d /etc/apt/keyrings
-curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
-chmod a+r /etc/apt/keyrings/docker.asc
+sudo apt update
+sudo apt install -y ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
 
 # 2. Add the repository to Apt sources
-tee /etc/apt/sources.list.d/docker.sources <<EOF
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
 Types: deb
 URIs: https://download.docker.com/linux/debian
 Suites: $(. /etc/os-release && echo "$VERSION_CODENAME")
@@ -18,11 +18,11 @@ Components: stable
 Signed-By: /etc/apt/keyrings/docker.asc
 EOF
 
-apt update
+sudo apt update
 
 # 3. Install docker
-echo "---===*** Install docker ***===---"
-apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo echo "---===*** Install docker ***===---"
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # 4. Check docker status
 # systemctl status docker
@@ -32,4 +32,3 @@ apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker
 /usr/sbin/usermod -aG docker $USER
 /usr/sbin/newgrp docker
 
-exit
