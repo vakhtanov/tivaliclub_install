8152f27870299b05a7a2582764ceb6954638419b
Andrey Vakhtanov <wahha@mail.ru>
2026-04-15 00:06:29 +0300

    Update start setup_docker_tv_mod.sh

--- nextcloud/start setup_docker_tv_mod.sh
+++ nextcloud/start setup_docker_tv_mod.sh
@@ -11,11 +11,11 @@ NC='\033[0m'
 
 # 0. Проверка, установлен ли Docker
 if command -v docker &> /dev/null; then
-    echo -e "${GREEN}Docker уже установлен. Версия: $(docker --version)${NS}"
+    echo -e "${GREEN}Docker уже установлен. Версия: $(docker --version)${NC}"
     exit 0
 fi
 
-echo -e "${YELLOW}Docker не найден. Начинаем установку...${NS}"
+echo -e "${YELLOW}Docker не найден. Начинаем установку...${NC}"
 
 # 1. Set up Docker's apt repository
 sudo apt update
@@ -37,7 +37,7 @@ EOF
 sudo apt update
 
 # 3. Install docker
-echo "${YELLOW}---===*** Install docker ***===---${NS}"
+echo "${YELLOW}---===*** Install docker ***===---${NC}"
 sudo apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 # 4. настройка прав
@@ -46,11 +46,11 @@ sudo usermod -aG docker $USER
 
 # 5. Вывод версии
 echo "------------------------------------------------------"
-echo "${GREEN}Установка завершена!${NS}"
+echo "${GREEN}Установка завершена!${NC}"
 echo -n "Установленная версия: "
 docker --version
 
 echo "------------------------------------------------------"
-echo "${YELLOW}ВАЖНО: Чтобы запускать контейнеры без sudo, выполните команду:${NS}"
-echo "${YELLOW}newgrp docker${NS}"
-echo "${YELLOW}Или перезайдите в систему.${NS}"
+echo "${YELLOW}ВАЖНО: Чтобы запускать контейнеры без sudo, выполните команду:${NC}"
+echo "${YELLOW}newgrp docker${NC}"
+echo "${YELLOW}Или перезайдите в систему.${NC}"
