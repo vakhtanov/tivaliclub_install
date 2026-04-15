06969b167b1d769b9cb34cdac87dd8abeb016b6f
Andrey Vakhtanov <wahha@mail.ru>
2026-04-15 17:33:10 +0300

    Update install_prometheus_stack.sh

--- prometheus_stack/test/install_prometheus_stack.sh
+++ prometheus_stack/test/install_prometheus_stack.sh
@@ -1,1 +1,13 @@
-# /usr/bin/bash
+#!/usr/bin/env bash
+set -euo pipefail ## ЗАВЕРШАЕТ БАШ СКПРИПТ ПРИ ЛЮБОЙ ОШИБКЕ!!!!!
+
+REPO_URL="https://github.com/hyperdxio/hyperdx.git"
+INSTALL_DIR="/opt/hyperdx"
+RUN_USER="hyperdx"
+SERVER_IP="hyperdx.tivaliclub.com"  # ← Твой домен!
+
+## КРАСИТ ЭХО В РАЗНЫЕ ЦВЕТА!!!!
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+NC='\033[0m'
