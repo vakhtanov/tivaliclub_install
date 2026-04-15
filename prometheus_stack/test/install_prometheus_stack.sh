01d82f603fbae0b56e8fa6c2f04278a7fe00c7b9
Andrey Vakhtanov <wahha@mail.ru>
2026-04-15 17:42:09 +0300

    Update install_prometheus_stack.sh

--- prometheus_stack/test/install_prometheus_stack.sh
+++ prometheus_stack/test/install_prometheus_stack.sh
@@ -2,7 +2,7 @@
 set -euo pipefail ## ЗАВЕРШАЕТ БАШ СКПРИПТ ПРИ ЛЮБОЙ ОШИБКЕ!!!!!
 
 REPO_URL="https://github.com/hyperdxio/hyperdx.git"
-INSTALL_DIR="/opt/hyperdx"
+INSTALL_DIR="/opt/prometheus_stack"
 RUN_USER="hyperdx"
 SERVER_IP="hyperdx.tivaliclub.com"  # ← Твой домен!
 
@@ -11,3 +11,8 @@ GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 RED='\033[0;31m'
 NC='\033[0m'
+
+
+#===================
+
+mkdir -p $INSTALL_DIR/{prometheus,grafana,alertmanager,blackbox}
