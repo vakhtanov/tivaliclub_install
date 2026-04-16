f3d3357ec1e02f91ada8f304b89726590583e072
Andrey Vakhtanov <wahha@mail.ru>
2026-04-16 19:12:36 +0300

    Update install_node_exporter.sh

--- prometheus_stack/client/install_node_exporter.sh
+++ prometheus_stack/client/install_node_exporter.sh
@@ -1,1 +1,32 @@
+#!/usr/bin/env bash
+set -euo pipefail ## EXIT SCRIPT ON ANY ERROR!
 
+INSTALL_DIR="/opt/prometheus_stack"
+#RUN_USER="hyperdx"
+#SERVER_IP="grafana.devopsdemo.ru"  # ← Твой домен!
+PROMETHEUS_SRV_REPO="https://raw.githubusercontent.com/vakhtanov/tivaliclub_install/refs/heads/main/prometheus_stack/client"
+DOCKER_REPO="https://raw.githubusercontent.com/vakhtanov/tivaliclub_install/refs/heads/main/docker"
+
+## КРАСИТ ЭХО В РАЗНЫЕ ЦВЕТА!!!!
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+
+#===================INIT=============
+
+echo -e "${GREEN}INIT PREPARE${NC}"
+#sudo mkdir -p $INSTALL_DIR
+sudo mkdir -p $INSTALL_DIR/prometheus/node_exporter
+sudo wget $PROMETHEUS_SRV_REPO/docker-compose.yml -O $INSTALL_DIR/prometheus/node_exporter/docker-compose.yml
+
+#=============SETUP DOCKER OFFICIAL==============================
+
+echo -e "${GREEN}SETUP DOCKER OFFICIAL${NC}"
+
+wget -O - $DOCKER_REPO/setup_docker_tv_mod.sh | bash
+
+#echo -e "${YELLOW}LOGOUT, LOGIN ${NC}"
+echo -e "${YELLOW}cd $INSTALL_DIR/prometheus/node_exporter and DO COMMAND ${NC}"
+echo -e "${YELLOW}docker compose up -d${NC}"
